a6c92def164b06a62fb62ca2c7f5d53835dbe5db
wang <licb8568182@163.com>
2019-02-20 02:29:47 +0800

    wang

---
+++ pages/index/index.js
@@ -0,0 +1,77 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: ['../../images/a.jpg', '../../images/a.jpg','../../images/a.jpg'],
+    contentItems:[
+      { img: '../../images/a.jpg', text:'这是第一块文字' },
+      { img: '../../images/a.jpg', text: '这是第二块文字' },
+      { img: '../../images/a.jpg', text: '这是第三块文字' },
+      { img: '../../images/a.jpg', text: '这是第四块文字' }
+    ],
+    listItem:[
+      { img: '../../images/a.jpg', text: '这是第一块文字' },
+      { img: '../../images/a.jpg', text: '这是第一块文字发动反击两地分发动机来访接待来访接待来访居发的老妇见到了费德勒' },
+      { img: '../../images/a.jpg', text: '这是第一块文字' }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
